9791e6a38d29349e6957f04a8da8cf2fd0791b4d
jacobp130 <146958089+jacobp130@users.noreply.github.com>
2024-03-18 13:54:22 +0000

    input

--- input.js
+++ input.js
@@ -7,15 +7,16 @@ let messages = document.querySelector(".messages");
 
 submitButton.addEventListener("click", function() {
     // 4. Declare a variable to store the value of the input
+  
     // 5. Use the console to check a value is saved.
     //  - Type something and click the button to confirm it works!
-
-
+let input= document.querySelector(".comment-input").value;
+console.log(input)
     
 
     // CODE ALONG, PART 2
     // 6. User .innerHTML to display the value to the messages div.
     // 7. Type something and test it out.
-
+messages.innerHTML=input;
 
 });
